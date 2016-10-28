f536fbfdd363a35104abd1063330921916124e59
💞 💗 <dpiet@salsify.com>
2016-10-28 18:32:30 -0400

    Simple yarn + react project with workflow

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yarn run build
+yarn run build-style
+yarn run build-static
+yarn run serve
---
+++ src/script.js
@@ -0,0 +1,5 @@
+var profile = <div>
+  <h1>Hello World</h1>
+</div>;
+
+ReactDOM.render(profile, document.getElementById('root'));
